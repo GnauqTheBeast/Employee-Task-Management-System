422574b396930310dfabe69210948af14e4333e7
Quang PC <quangnguyenngoc314@gmail.com>
2024-05-18 11:35:50 +0700

    first commit

---
+++ ems_header.php
@@ -0,0 +1,15 @@
+
+<div class="row">
+<div class="gap"></div>
+<div class="gap"></div>
+  <div class="col-md-12">
+
+    <div class="well">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <marquee><span style="color: green;"> Xin chào <strong><?php echo $user_name; ?></strong>. Chào mừng tới <strong> Quản lý nhân viên</strong></span></marquee>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
